17c67739211f8213b5cf3521de93e8d5658a0a56
dimashirda <dimas0308@gmail.com>
2017-12-04 22:16:34 +0700

    buyar buyar tak balekno

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,8 +15,6 @@ class HomeController extends Controller
     	$id = $request->session()->get('id');
     	$name = $request->session()->get('username');
         //dd($id);
-        if($name != 'admin')
-        {
             $query = DB::select("SELECT users.kunci as kunci , 
                         users.IV as iv FROM users WHERE users.id = '".$id."'"); 
             $key = $query[0]->kunci;
@@ -41,54 +39,6 @@ class HomeController extends Controller
                     ));
             }
             return view('start', compact('rec'));    
-        }
-        else
-        {
-            $query = DB::select("SELECT * 
-                        FROM users WHERE users.id != '".$id."'");
-               //dd($query);
-                $pair = array(
-                    'id' => $id_pair = array(),
-                    'key' => $key_pair = array(),
-                    'IV' => $iv_pair = array(),
-                    );
-                foreach ($query as $tmp) {
-                    array_push($pair['id'],$tmp->id);
-                    array_push($pair['key'],$tmp->kunci);
-                    array_push($pair['IV'],$tmp->IV);
-                }
-            //dd($pair);
-            $num_pair = count($pair['id']);
-            //dd($num_pair);
-            for($i=0;$i<$num_pair;$i++)
-            {
-                $rec[$i] = DB::select("SELECT * FROM record 
-                WHERE record.id_user = '".$pair['id'][$i]."' 
-                ORDER BY created_at DESC");
-                //dd($rec);
-                $num_rec = count($rec[$i]);
-                for($j=0;$j<$num_rec;$j++)
-                {
-                    $rec[$i][$j]->judul_transaksi = $this->dekrip(openssl_decrypt(
-                        $rec[$i][$j]->judul_transaksi,
-                        'AES-256-CBC',
-                        $pair['key'][$i],
-                        0,
-                        $pair['IV'][$i]
-                        ));
-                    $rec[$i][$j]->tempat = $this->dekrip(openssl_decrypt(
-                        $rec[$i][$j]->tempat,
-                        'AES-256-CBC',
-                        $pair['key'][$i],
-                        0,
-                        $pair['IV'][$i] 
-                        ));
-                }
-                //dd($rec);
-            }
-            //dd($rec);
-            return view('start', compact('rec'));
-        }
     }
     public function dekrip($data)
     {
